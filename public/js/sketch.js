684b9db1abb74c666993b25890a19b86dd55a45d
ben <brmscheiner@gmail.com>
2016-04-27 14:00:11 -0400

    Shapes and shape sizes

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -8,6 +8,8 @@ function setup(){
     var canvasContainer = document.getElementById("canvasContainer");
     myCanvas.parent(canvasContainer);
     colorMode(HSB, 100);
+    rectMode(CENTER);
+    ellipseMode(CENTER);
     background(235);
 
     initH = random(100);
@@ -34,16 +36,30 @@ function setup(){
 
 
 function draw() {
+    if (p5controller.current_shape === "line") {
+      if (mouseIsPressed) {
+          drawStroke(pmouseX, pmouseY, mouseX, mouseY);
+          //noCursor();
+      }
+    } else if (p5controller.current_shape === "rect") {
+      if (mouseIsPressed) {
+        colorMode(RGB, 255);
+        fill(color('#'+p5controller.current_color));
+        stroke(color('#'+p5controller.current_color));
+        rect(mouseX, mouseY, p5controller.shape_size, p5controller.shape_size);
+      }
+    } else if (p5controller.current_shape === "circ") {
     if (mouseIsPressed) {
-        drawStroke(pmouseX, pmouseY, mouseX, mouseY);
-        //noCursor();
+      colorMode(RGB, 255);
+      fill(color('#'+p5controller.current_color));
+      stroke(color('#'+p5controller.current_color));
+      ellipse(mouseX, mouseY, p5controller.shape_size, p5controller.shape_size);
     }
-    lastMouseX = mouseX;
-    lastMouseY = mouseY;
+  }
 }
 
 drawStroke = function(xVal, yVal, hVal, rVal){
    colorMode(RGB, 255);
-   stroke(color('#'+current_color));
+   stroke(color('#'+p5controller.current_color));
    line(xVal, yVal, hVal, rVal);
 }
