67cf5da24c5232fe3ef381e982ba57d184e55f21
ben <brmscheiner@gmail.com>
2016-04-28 01:36:45 -0400

    color transitions

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -34,18 +34,27 @@ function makeConnectors() {
 
 function draw() {
     var pct = 1;
-    fill(color('#'+p5controller.current_color));
-    stroke(color('#'+p5controller.current_color));
-
+    if (p5controller.total_time > 0) {
+      pct = 1 - p5controller.timer/p5controller.total_time;
+    }
+    // fill(color('#'+p5controller.current_color));
+    // stroke(color('#'+p5controller.current_color));
+    var last_color = color('#'+p5controller.last_color);
+    var current_color = color('#'+p5controller.current_color);
+    var r = pct * (last_color.levels[0] - current_color.levels[0]) + current_color.levels[0];
+    var g = pct * (last_color.levels[1] - current_color.levels[1]) + current_color.levels[1];
+    var b = pct * (last_color.levels[2] - current_color.levels[2]) + current_color.levels[2];
+    fill(color(r,g,b));
+    stroke(color(r,g,b));
+    
     if (p5controller.clear) {
       p5controller.clear = false;
       background(235);
     }
 
     if (mouseIsPressed) {
-      if (p5controller.timer > 0  && p5controller.total_time > 0) {
+      if (p5controller.timer > 0) {
         p5controller.timer -= 1; // or else shapes will continue changing size
-        pct = 1 - p5controller.timer/p5controller.total_time;
       } else if (p5controller.timer === 0) {
         p5controller.timer = p5controller.total_time;
       }
