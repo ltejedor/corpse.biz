ba21134885fe853c1c5351860d00ba0515d62296
ben <brmscheiner@gmail.com>
2016-05-03 00:31:54 -0400

    shadow canvas for live preview

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,75 +1,49 @@
-var initH, initS, initB, initR, initX, initY;
-initS = 130;
-initB = 200;
-var step = 20;
+var sketch_p5 = new p5(function(sketch) {
 
-function setup(){
-    var myCanvas = createCanvas(700, 500);
-    var canvasContainer = document.getElementById("canvasContainer");
-    myCanvas.parent(canvasContainer);
-    colorMode(RGB);
-    rectMode(CENTER);
-    ellipseMode(CENTER);
-    background(235);
-
-    initH = random(100);
-    initR = random(10, 40);
-
-    initX = random(0, width);
-    initY = random(0, height);
-
-
-}
-
-function makeConnectors() {
-  strokeWeight(3);
-  stroke(75);
-  var initLineYTop = height/2 - 5;
-  var initLineYBottom = height/2 + 5;
-  for(var i = 0; i < 6; i++){
-    var initX = random(0, width);
-    line(initX, initLineYTop, initX, initLineYBottom);
-  }
-}
-
-function draw() {
-    var pct = 1;
-    if (p5controller.total_time > 0) {
-      pct = 1 - p5controller.timer/p5controller.total_time;
-    }
-    // fill(color('#'+p5controller.current_color));
-    // stroke(color('#'+p5controller.current_color));
-    var last_color = color('#' + p5controller.last_color);
-    var current_color = color('#' + p5controller.current_color);
-    var r = pct * (last_color.levels[0] - current_color.levels[0]) + current_color.levels[0];
-    var g = pct * (last_color.levels[1] - current_color.levels[1]) + current_color.levels[1];
-    var b = pct * (last_color.levels[2] - current_color.levels[2]) + current_color.levels[2];
-    fill(color(r,g,b));
-    stroke(color(r,g,b));
-    
-    if (p5controller.clear) {
-      p5controller.clear = false;
-      background(235);
+    sketch.setup = function() {
+        var myCanvas = sketch.createCanvas(700, 500);
+        var canvasContainer = document.getElementById("canvasContainer");
+        myCanvas.parent(canvasContainer);
+        sketch.colorMode(sketch.RGB);
+        sketch.rectMode(sketch.CENTER);
+        sketch.ellipseMode(sketch.CENTER);
+        sketch.background(235);
+        sketch.makeConnectors();
     }
 
-    if (mouseIsPressed) {
-      if (p5controller.timer > 0) {
-        p5controller.timer -= 1; // or else shapes will continue changing size
-      } else if (p5controller.timer === 0) {
-        p5controller.timer = p5controller.total_time;
+    sketch.makeConnectors = function() {
+      sketch.strokeWeight(3);
+      sketch.stroke(75);
+      var initLineYTop = sketch.height/2 - 5;
+      var initLineYBottom = sketch.height/2 + 5;
+      for(var i = 0; i < 6; i++){
+        var initX = sketch.random(0, sketch.width);
+        sketch.line(initX, initLineYTop, initX, initLineYBottom);
       }
+    }
 
-      if (p5controller.current_shape === "line") {
-        var transition_weight = 0.1 * pct * (p5controller.size - p5controller.last_size) + p5controller.last_size;
-        strokeWeight(transition_weight);
-        line(pmouseX, pmouseY, mouseX, mouseY);
-      } else if (p5controller.current_shape === "rect") {
-        var transition_size = pct * (p5controller.size - p5controller.last_size) + p5controller.last_size;
-        rect(mouseX, mouseY, transition_size, transition_size);
-      } else if (p5controller.current_shape === "circ") {
-        var transition_size = pct * (p5controller.size - p5controller.last_size) + p5controller.last_size;
-        ellipse(mouseX, mouseY, transition_size, transition_size);
+    sketch.draw = function() {
+        var currentSize = p5controller.size;
+        var currentColor = sketch.color('#' + p5controller.color);
+        var currentShape = p5controller.shape;
+        sketch.fill(currentColor);
+        sketch.stroke(currentColor);
+        sketch.strokeWeight(currentSize);
+        
+        if (p5controller.clear) {
+          p5controller.clear = false;
+          sketch.background(235);
+        }
+
+        if (sketch.mouseIsPressed) {
+          if (p5controller.shape === "line") {
+            sketch.line(sketch.pmouseX, sketch.pmouseY, sketch.mouseX, sketch.mouseY);
+          } else if (p5controller.shape === "rect") {
+            sketch.rect(sketch.mouseX, sketch.mouseY, currentSize, currentSize);
+          } else if (p5controller.shape === "circ") {
+            sketch.ellipse(sketch.mouseX, sketch.mouseY, currentSize, currentSize);
+          }
+
       }
-
-  }
-}
+    }
+})
